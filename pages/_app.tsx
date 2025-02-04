b81d3afa751724412692ad57c99c0ed3cf197bce
Alyssa Feola <65581330+its-a-lisa@users.noreply.github.com>
2025-02-04 00:56:43 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import '@/styles/globals.css';
 import { PlasmicRootProvider } from "@plasmicapp/react-web";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import '../plasmic-init.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
