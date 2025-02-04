b8c6158e42fe74445282568ca3dc2639d960fc20
Alyssa Feola <65581330+its-a-lisa@users.noreply.github.com>
2025-02-04 00:45:39 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,17 @@ import '@/styles/globals.css'
 import { PlasmicRootProvider } from "@plasmicapp/react-web";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import '../plasmic-init.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <PlasmicRootProvider Head={Head}>
+    <PlasmicRootProvider 
+      Head={Head}
+        <link 
+          rel="stylesheet" 
+          href="https://fonts.googleapis.com/css2?family=YourFontName&display=swap" 
+        />
+      >
       <Component {...pageProps} />
     </PlasmicRootProvider>
   );
